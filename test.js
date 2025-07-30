afb62121a936f2a1ea7bf816c89405967f272153
Najla Noori <najla.n1386@gmail.com>
2025-07-29 23:03:37 -0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,23 @@
+
+age = 24;      // Update value without 'let' or 'const'
+console.log(age);
+console.log("External JavaScript file loaded successfully.");
+
+var externalVariable = "This is an external variable";
+var test = "Test variable";
+console.log(externalVariable);
+
+let teacher_name = "Najla";
+
+
+
+function greet() {
+  let city = "Badakhshan";
+  console.log("Hello from the external JavaScript file function!");
+}
+for (let i = 0; i < 5; i++) 
+  if (age > 18) 
+    console.log("You are an adult.");
+   else 
+    console.log("You are not an adult.")
+  
